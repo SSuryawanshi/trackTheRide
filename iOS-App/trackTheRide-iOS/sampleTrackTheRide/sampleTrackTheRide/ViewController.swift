9aa430d2d0f9a238703a30a026da639d73a528cf
Srikanth Suryawanshi <srikanth.sp9@gmail.com>
2016-11-28 20:48:54 +0530

    added UI for start & stop Trip.

--- iOS-App/trackTheRide-iOS/sampleTrackTheRide/sampleTrackTheRide/ViewController.swift
+++ iOS-App/trackTheRide-iOS/sampleTrackTheRide/sampleTrackTheRide/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startTripBtn: UIButton!
+    
+    @IBOutlet weak var stopTripBtn: UIButton!
+    
+    @IBOutlet weak var tripStatusTextF: UITextField!
+    
+    @IBOutlet weak var tripDetailsTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
